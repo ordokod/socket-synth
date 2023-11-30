7965331d66116b49a8b4198e3fff47043e30794f
martinfriman <martin@hajapa.se>
2023-11-30 11:13:50 +0100

    minor updates

--- public/js/host.js
+++ public/js/host.js
@@ -1,6 +1,14 @@
 import MIDI from "./midi.js";
 import { emitNoteToServer, joinAsHost } from "./socket.js";
 
+const isTouchDevice = () => {
+  return (
+    "ontouchstart" in window ||
+    navigator.maxTouchPoints > 0 ||
+    navigator.msMaxTouchPoints > 0
+  );
+};
+
 const updatePianoKeys = (players) => {
   players.forEach((player) => {
     const key = document.querySelector(`.key[data-key="${player.note}"]`);
@@ -8,25 +16,35 @@ const updatePianoKeys = (players) => {
   });
 };
 
-joinAsHost({
-  onNotePlayed: (note) => {},
-  onClientsChanged: (clients) => {
-    updatePianoKeys(clients);
-  },
-});
+const addListeners = () => {
+  const keys = document.querySelectorAll(".key");
 
-const keys = document.querySelectorAll(".key");
+  if (isTouchDevice()) {
+    keys.forEach((key) => {
+      key.addEventListener("touchstart", (e) => {
+        const note = parseInt(e.target.dataset.key);
+        playNote({ note, velocity: 100 });
+      });
 
-keys.forEach((key) => {
-  key.addEventListener("mousedown", (e) => {
-    const note = parseInt(e.target.dataset.key);
-    playNote({ note, velocity: 100 });
+      key.addEventListener("touchend", (e) => {
+        const note = parseInt(e.target.dataset.key);
+        playNote({ note, velocity: 0 });
+      });
+    });
+    return;
+  }
+
+  keys.forEach((key) => {
+    key.addEventListener("mousedown", (e) => {
+      const note = parseInt(e.target.dataset.key);
+      playNote({ note, velocity: 100 });
+    });
+    key.addEventListener("mouseup", (e) => {
+      const note = parseInt(e.target.dataset.key);
+      playNote({ note, velocity: 0 });
+    });
   });
-  key.addEventListener("mouseup", (e) => {
-    const note = parseInt(e.target.dataset.key);
-    playNote({ note, velocity: 0 });
-  });
-});
+};
 
 const playNote = (note) => {
   const key = document.querySelector(`.key[data-key="${note.note}"]`);
@@ -35,8 +53,19 @@ const playNote = (note) => {
   emitNoteToServer(note);
 };
 
+document.getElementById("piano").style.transform = "scale(2)";
+
+addListeners();
+
 MIDI({
   onNotePlayed: (note) => {
     playNote(note);
+  },
+});
+
+joinAsHost({
+  onNotePlayed: (note) => {},
+  onClientsChanged: (clients) => {
+    updatePianoKeys(clients);
   },
 });
